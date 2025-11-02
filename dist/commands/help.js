e57986e0401de17e73cb57cd764f391a99360a6a
deakay13 <nguyenleduykhang13@gmail.com>
2025-11-02 15:52:23 +0700

    fĩ: flashcommands help

--- dist/commands/help.js
+++ dist/commands/help.js
@@ -1,16 +1,21 @@
-import { Message, EmbedBuilder, TextChannel } from "discord.js";
+import { Message } from "discord.js";
 export const name = "Help";
 export async function execute(message, args) {
-    const embed = new EmbedBuilder()
-        .setColor(0x00ffcc)
-        .setTitle("📘 Trợ lý ảo Kon")
-        .setDescription("Đây là thông báo từ trợ lý ảo của bạn. Hãy nhập lệnh để được giúp đỡ!")
-        .setFooter({ text: "Agent Bot • 2025" });
-    // Kiểm tra kênh có phải là TextChannel không
-    if (message.channel instanceof TextChannel) {
-        await message.channel.send({ embeds: [embed] });
-    }
-    else {
-        console.log("Không thể gửi embed trong loại kênh này.");
-    }
+    const embed = {
+        color: 0x00ffcc,
+        title: `Thông tin của Dog`,
+        fields: [
+            {
+                name: "Hello",
+                value: "Gâu Gâu",
+                inline: true,
+            },
+            {
+                name: "📅 Đây là Dog",
+                value: "Dog là trợ lý ảo dễ thương.",
+                inline: true,
+            },
+        ],
+    };
+    await message.reply({ embeds: [embed] });
 }
